0e455c9a39b1515584f8632a2dcbf87d5c7d8c0c
adefemisultan <adefemisultan808@gmail.com>
2021-04-04 07:08:01 -0700

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAQ_79KgQUsAWEXlY8f9cz2n3pJhHvWroI",
+    authDomain: "tinder-clone-7099c.firebaseapp.com",
+    projectId: "tinder-clone-7099c",
+    storageBucket: "tinder-clone-7099c.appspot.com",
+    messagingSenderId: "481132120885",
+    appId: "1:481132120885:web:8bc16a4d68b361d4249a7c"
+  };
